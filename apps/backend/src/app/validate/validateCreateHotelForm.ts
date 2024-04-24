7fa77ad43bf32cbc09987da2a9b4c921227f1452
bahungv <71269151+VuBaHung@users.noreply.github.com>
2024-04-24 14:02:51 +0700

    validate input with Joi

--- apps/backend/src/app/validate/validateCreateHotelForm.ts
+++ apps/backend/src/app/validate/validateCreateHotelForm.ts
@@ -3,5 +3,5 @@ export const createHotelSchema = Joi.object({
   name: Joi.string().required(),
   hotelId: Joi.string().required(),
   image: Joi.object(),
-  descript: Joi.string().min(0),
+  descript: Joi.string().max(200)
 });
